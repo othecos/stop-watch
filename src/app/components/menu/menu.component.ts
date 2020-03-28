867d0b260ac4cc4ce7c9889e90df243524f8b363
othecos <othecos@gmail.com>
2020-03-28 12:28:33 -0300

    SHow list only on Interval Training

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -15,6 +15,7 @@ export class MenuComponent implements OnInit {
   public selectedIndex = 0;
 
   appPages: Array<Pages> = []
+  showExerciseList: boolean = false
   constructor(
     private router: Router,
     private pagesServices: PagesService,
@@ -26,13 +27,25 @@ export class MenuComponent implements OnInit {
     let subscription = this.router.events.subscribe((routerEvent:RouterEvent) => {
       const path = window.location.pathname.split('/')[1]
       if (path !== undefined) {
-        if(this.setSelectedIndex(path) != -1)  subscription.unsubscribe()
+         this.setSelectedIndex(path)
       }
+      this.showExerciseList = Utils.removeSpecialCharacter(path) == 'interval'
     })
+    
   }
 
   private setSelectedIndex(path:string){
     this.selectedIndex = this.appPages.findIndex(page => Utils.removeSpecialCharacter(page.url.toLowerCase()) === path.toLowerCase());
     return this.selectedIndex
   }
+  isListVisible(){
+    return this.showExerciseList
+  }
+  getCurrentPage(path){
+    if(!path) path = window.location.pathname.split('/')[1]
+    let currentPage = this.appPages.find(page => Utils.removeSpecialCharacter(page.url.toLowerCase()) === path.toLowerCase());
+    console.log(currentPage)
+
+  }
+  return 
 }
