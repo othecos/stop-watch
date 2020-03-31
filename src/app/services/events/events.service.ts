4daa789ef0cea6624befb0aa5ee1b3fc1cb88edf
othecos <othecos@gmail.com>
2020-03-30 21:40:14 -0300

    Auto-generated commit

--- src/app/services/events/events.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, EventEmitter } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class EventsService {
-  public menuEvent:EventEmitter<'close' | 'open'> = new EventEmitter()
-  constructor() { }
-}
