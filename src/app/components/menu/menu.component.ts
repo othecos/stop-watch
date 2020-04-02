94ee2747d8ea92f82735d06e9463e85b9daafe94
othecos <othecos@gmail.com>
2020-04-02 12:21:20 -0300

    bugs fix

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -17,9 +17,7 @@ export class MenuComponent implements OnInit {
   appPages: Array<Pages> = []
   showExerciseList: boolean = false
   constructor(
-    private router: Router,
     private pagesServices: PagesService,
-    private activatedRoute:ActivatedRoute
   ) {
     this.appPages = this.pagesServices.appPages
   }
@@ -30,18 +28,10 @@ export class MenuComponent implements OnInit {
   }
   checkVisibility(){
     this.showExerciseList = this.isPageActive('interval')
-    console.log(this.pagesServices.getActivePage());
-    
   }
   isPageActive(URL){
     try{
       return this.pagesServices.getActivePage().url.includes(URL) 
     }catch{return false}
-  }
-  getCurrentPage(path){
-    if(!path) path = window.location.pathname.split('/')[1]
-    let currentPage = this.appPages.find(page => Utils.removeSpecialCharacter(page.url.toLowerCase()) === path.toLowerCase());
-    console.log(currentPage)
-
   }
 }
