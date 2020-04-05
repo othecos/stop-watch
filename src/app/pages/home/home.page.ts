876f09c22d1446e21dcc05c15202f59e1f105a6f
othecos <othecos@gmail.com>
2020-04-05 16:08:49 -0300

    Home page responsiviness fix

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,33 +1,51 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
 import { PagesService } from 'src/app/services/pages/pages.service';
 import { Pages } from 'src/app/services/pages/pages.models';
 import { MenuController, IonSplitPane, IonSlides } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit,OnDestroy, AfterViewInit{
   appPages:Array<Pages>
   isMenuOpen:boolean = false;
+  animateMenuButton = false;
   @ViewChild('slide',{static: false}) slide:IonSlides
+  subscription:Array<Subscription> = []
   constructor(
     private pagesService: PagesService,
     public menuController: MenuController,
     public splitPaneController:IonSplitPane
   ) {
-   
+    
    }
 
   async ngOnInit() {
     this.appPages = this.pagesService.appPages;
   }
+  ngAfterViewInit(){
+    this.onLastPage()
+  }
   getRoute(name){
     return this.pagesService.getRoute(name)
+  }
+  onMenuButtonClicked(){
+    this.animateMenuButton =  false
+  }
+  onLastPage(){
+   let subs =  this.slide.ionSlideReachEnd.subscribe((isEnd)=>{
+      console.log(isEnd);
+      this.animateMenuButton = true
+    })
+    this.subscription.push(subs)
   }
   async onOpenMenu(){
     this.menuController.open('menu')
   }
-
+  ngOnDestroy(){
+    this.subscription.forEach((subs:Subscription)=> subs.unsubscribe())
+  }
 }
